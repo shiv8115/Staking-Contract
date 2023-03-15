2977e9295db319f26482baacab10f7d122281618
Shivam Singh <sk811586@gmail.com>
2023-03-15 14:32:59 +0530

    prettier

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-chai-matchers");
-require('@openzeppelin/hardhat-upgrades');
+require("@openzeppelin/hardhat-upgrades");
 require("hardhat-deploy");
-require('solidity-coverage');
+require("solidity-coverage");
 require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -11,13 +11,12 @@ module.exports = {
   solidity: "0.8.18",
   namedAccounts: {
     deployer: {
-        default: 0, // ethers built in account at index 0
+      default: 0, // ethers built in account at index 0
     },
-},
-etherscan: {
-  // Your API key for Etherscan
-  // Obtain one at https://etherscan.io/
-  apiKey: "CIQMG8MEB51SCJJ6W7AMNGIS3MAFV8B6U9"
-},
-
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "CIQMG8MEB51SCJJ6W7AMNGIS3MAFV8B6U9",
+  },
 };
